9fe3d17e1fce9d77f7ee7fcada7cd4375026e7d8
Rahul Kumar <messirahul96@gmail.com>
2017-07-05 12:32:28 +0530

    Update ex1_10.scm
    Added case for (A 2 n), when n > 2;

--- scheme/chapter1/ex1_10.scm
+++ scheme/chapter1/ex1_10.scm
@@ -23,5 +23,6 @@
 
 (define (f n) (A 0 n))                  ; 2*n
 (define (g n) (A 1 n))                  ; 2^n
-(define (h n) (A 2 n))                  ; 2^(2^(n-1))
-(define (k n) (* 5 n n))                ; 5n^2
+(define (h n) (A 2 n))                  ; { 2^(2^(n-1)) , n = 1 or n = 2;
+                                        ;   2^2^n ,       otherwise;     }
+(define (k n) (* 5 n n))                ; 5*(n^2)
