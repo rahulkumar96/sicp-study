c0b21e99db7495e0de8c979c506bc0923fe60aaa
Rahul Kumar <messirahul96@gmail.com>
2017-07-05 12:57:05 +0530

    Update ex1_10.scm
    Added case for n = 1 in (A 2 n)

--- scheme/chapter1/ex1_10.scm
+++ scheme/chapter1/ex1_10.scm
@@ -23,6 +23,15 @@
 
 (define (f n) (A 0 n))                  ; 2*n
 (define (g n) (A 1 n))                  ; 2^n
-(define (h n) (A 2 n))                  ; { 2^(2^(n-1)) , n = 1 or n = 2;
-                                        ;   2^2^n ,       otherwise;     }
+(define (h n) (A 2 n))                  ; { 2 ,           n = 1;
+                                        ;   2^(h (n-1)) ,  otherwise;}
 (define (k n) (* 5 n n))                ; 5*(n^2)
+
+;; (h n) explanaion:
+;; (h 1) = 2;
+;; (h 2) = 2^2 = 4;
+;; (h 3) = 2^4 = 16;
+;; (h 4) = 2^16 = 65536;
+;; (h 5) = 2^65563; (I was amazed to see Dr. Scheme interpreter print that number!!!!)
+
+;; (h n) = 2^(h n-1);
